270bb78eea7949ee361bf45fc48e89e988586115
Bikram Saha <84061957+imbikramsaha@users.noreply.github.com>
2021-08-28 14:24:17 +0530

    Update QRcode.py

--- QRcode.py
+++ QRcode.py
@@ -1,4 +1,5 @@
-import qrcode
+# pip install qrcode ( at first install qrcode in windows powershell or command prompt or your mac / linux Terminal )
 
+import qrcode
 img = qrcode.make("imbikramsaha@zohomail.in")
 img.save("Zohomail.jpg")
